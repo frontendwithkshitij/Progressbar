b39da23b9b3fdc1df4c737ce6e6f678045edfd7c
Kshitij <frontendwithkshitij@gmail.com>
2024-06-28 18:46:35 +0530

    refactor code

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-function move() {
+function progress() {
     let element = document.getElementById("myBar");
     let button = document.getElementById("myButton");
     let width = 0;
     button.disabled = true; // Disable the button
     let id = setInterval(function() {
-        frame(element, width++, id, button);
+        checkInterval(element, width++, id, button);
     }, 10);
 }
 
-function frame(element, width, id, button) {
-    if (width >= 100) {
+function checkInterval(element, width, id, button) {
+    if (width >= 101) {
         clearInterval(id);
         button.disabled = false; // Re-enable the button when done
     } else {
